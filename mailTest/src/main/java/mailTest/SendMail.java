eaccdf4ac5f0cffbd1c56f41c37b64f3d81eef5d
yulim <yulim13sky@naver.com>
2023-07-02 21:12:36 +0900

    feat: naver mail 인증

---
+++ mailTest/src/main/java/mailTest/SendMail.java
@@ -0,0 +1,56 @@
+package mailTest;
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMail {
+
+    public static void main(String[] args) {
+        naverMailSend();
+    }
+
+    public static void naverMailSend() {
+        String host = "smtp.naver.com"; // 네이버 SMTP 서버
+        String user = "yu_limmi_@naver.com"; // 네이버 계정
+        String password = "실제 내 비밀번호"; // 네이버 계정 비밀번호
+
+        // SMTP 서버 정보를 설정한다.
+        Properties props = new Properties();
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.naver.com");
+
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, password);
+            }
+        });
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(user));
+            message.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress("yu_limmi_@naver.com"));
+
+            // 메일 제목
+            message.setSubject("SMTP TEST");
+
+            // 메일 내용
+            message.setText("TEST Success!!");
+
+            // 메일 전송
+            Transport.send(message);
+            System.out.println("Success Message Sent");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ mailTest/src/main/webapp/main.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>mailTest</title>
+</head>
+<body>
+	<h1>Mail Test입니다.</h1>
+	<form action="loginOk.jsp" method="post">
+		이메일: <input type="text" name="email"><br />
+		<input type="submit" value="전송">&nbsp;&nbsp;
+	</form>
+</body>
+</html>
